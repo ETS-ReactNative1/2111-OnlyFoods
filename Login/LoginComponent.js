6164a3c1fc9612beb25b561fb9065ae4d9567658
Melissa Moy <melissa.e.moy@gmail.com>
2022-01-31 15:14:37 -0500

    moved stylesheet to LoginStyle.js

--- Login/LoginComponent.js
+++ Login/LoginComponent.js
@@ -9,6 +9,7 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
+import styles from "./LoginStyle"
 
 function LoginComponent() {
   return (
@@ -64,45 +65,5 @@ function LoginComponent() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  signupContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 30,
-  },
-  logo: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-  },
-  container: {
-    backgroundColor: "white",
-    paddingTop: 50,
-    paddingHorizontal: 12,
-  },
-  wrapper: {
-    marginTop: 80,
-  },
-  input: {
-    borderRadius: 4,
-    padding: 12,
-    backgroundColor: "#FAFAFA",
-    marginBottom: 10,
-    borderWidth: 1,
-  },
-  button: {
-    backgroundColor: "#0096F6",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: 42,
-    borderRadius: 4,
-  },
-  buttonText: {
-    fontWeight: "600",
-    color: "#fff",
-    fontSize: 20,
-  },
-});
 
 export default LoginComponent;
