dac20ceeeb9f531bb9f774c4a87e42e582d95d62
Melissa Moy <melissa.e.moy@gmail.com>
2022-02-01 15:44:45 -0500

    connected Login/Signup views to firebase, set up preliminary screen

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { Button, ImageBackground, Text, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from 'react'
+import { Button, ImageBackground, Text, SafeAreaView, View } from "react-native";
 import LoginComponent from "./Login/LoginComponent";
+import SignupComponent from "./Signup/SignupComponent";
+import Homepage from './Homepage/HomepageComponent';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+//import { LoginScreen, HomeScreen, RegistrationScreen } from './screens'
+import { getAuth } from "firebase/auth";
+import { firebase } from "./firebase_config";
+
+
+// import {decode, encode} from 'base-64'
+// if (!global.btoa) {  global.btoa = encode }
+// if (!global.atob) { global.atob = decode }
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  // const [loading, setLoading] = useState(true)
+  // const [user, setUser] = useState(null)
+
   return (
-    <SafeAreaView>
-      {/* <Text>Hello World</Text>
-      <Text>This is a test</Text>
-      <Text>Lets make this work</Text> */}
-      <LoginComponent />
-    </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginComponent} />
+          <Stack.Screen options={{ headerShown: false }} name="SignUp" component={SignupComponent} />
+          <Stack.Screen options={{ headerShown: false }} name="Homepage" component={Homepage} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
