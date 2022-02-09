b0039bd189508d6a33e15cf615a591faba900fed
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-02-09 16:37:31 -0500

    last min changes

--- Screens/RecipeCardForHomeAndBookmarks/RecipeCard.js
+++ Screens/RecipeCardForHomeAndBookmarks/RecipeCard.js
@@ -40,7 +40,7 @@ const RecipeCard = ({ navigation, recipe, index, loggedInUser }) => {
   const [foodColor, setFoodColor] = useState(false);
   const [bookmarked, setBookmarked] = useState(false);
   const [recipeCardBookmarks, setRecipeCardBookmarks] = useState(null);
-  const [ImageURL, setImageURL] = useState('')
+  const [ImageURL, setImageURL] = useState("");
 
   const foodPressed = () => {
     setFoodColor(!foodColor);
@@ -87,7 +87,8 @@ const RecipeCard = ({ navigation, recipe, index, loggedInUser }) => {
 
       if (hasRecipe) setBookmarked(true);
 
-      if(recipe.ImageURL === '') setImageURL('https://i.imgur.com/tIrGgMa.png')
+      if (recipe.ImageURL === "")
+        setImageURL("https://i.imgur.com/tIrGgMa.png");
     }
   }, [bookmarks]);
 
@@ -142,7 +143,7 @@ const RecipeCard = ({ navigation, recipe, index, loggedInUser }) => {
                   style={{
                     fontWeight: "bold",
                     textTransform: "capitalize",
-                    fontSize: 20,
+                    fontSize: 16,
                     justifyContent: "center",
                     alignContent: "center",
                     marginLeft: 5,
@@ -204,6 +205,10 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     width: 330,
     height: 200,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
   },
   icons: {
     flexDirection: "row",
